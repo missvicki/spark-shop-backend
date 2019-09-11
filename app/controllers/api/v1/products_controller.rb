bc4ee2ca217795f91520d8701d4cfe982b100a89
BasilMawejje <basil.mawejje@gmail.com>
2019-09-11 18:22:43 +0300

    Add create method to products controller Add api namespace to products controller Add product create route

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ApplicationController
+class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
     render json: @products
@@ -7,5 +7,21 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: 201
+    else
+      render error: @product.errors, status: 422
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :unique_code, :price)
   end
 end
